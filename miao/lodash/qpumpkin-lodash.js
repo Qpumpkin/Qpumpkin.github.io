061dbc74a6a2f55ce485082d7feca2aee6ac7530
Qpumpkin <wangboming11@hotmail.com>
2018-07-12 18:20:11 +0800

    lodash

--- miao/lodash/qpumpkin-lodash.js
+++ miao/lodash/qpumpkin-lodash.js
@@ -20,4 +20,55 @@ var qpumpkin = {
 
     return res;
   },
+  compact:
+  function compact(array) {
+    let res = [];
+    for (let i=0; i<array.length; i++) {
+      array[i] && res.push(array[i]);
+    }
+    return res;
+  },
+  concat:
+  function concat() {
+    let arg = [];
+    for (let i=0; i<arguments.length; i++) {
+      arg.push(arguments[i]);
+    }
+    let res = deconstruct(arg);
+    return res;
+
+    function deconstruct(arr) {
+      if (arr instanceof Array) {
+        let res = [];
+        let unit;
+        for (let i=0; i<arr.length; i++) {
+          unit = arr[i];
+          if (unit instanceof Array) {
+            unit = deconstruct(unit);
+            res.push(...unit);
+          } else {
+            res.push(unit);
+          }
+        }
+        return res;
+      } else {
+        return [arr];
+      }     
+    }
+  },
+  difference:
+  function difference(array,values) {
+    let res = [];
+    let map = new Set();
+    for (let value of values) {
+      !map.has(value) && map.add(value);
+    }
+
+    let cur;
+    for (let i=0; i<array.length; i++) {
+      cur = array[i];
+      !map.has(cur) && res.push(cur);
+    }
+    return res;
+  },
 }
